1725ad19502de4d2a7081f5c940120467a1f673a
rjpontel <rjpontello@ra.rockwell.com>
2025-03-13 16:22:09 -0400

    Can you figure out what I did

--- ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -5,38 +5,18 @@ using UAManagedCore;
 // WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
 //-------------------------------------------
 
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "01a7e8933afd7c95fe894b0859308138")]
 public class MainWindow : FTOptix.UI.Window
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
-public class DateAndTimeWidget : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
-public class NetworkSettingsWidget : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "4d7abd18b3a74f6037333cb23cbdd47f")]
-public class _3_ScreenHeaterRunControlDisplay : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "52b045d520e151468cb70a63d1b67c66")]
-public class Footer_Navigation1 : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
-public class LAN : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "72af352b5396ce47d1f7e3293b653c5b")]
-public class Panel_Screen_Heater_Control : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
 {
 }
 
@@ -45,23 +25,43 @@ public class Fault_button : FTOptix.UI.Rectangle
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "53b1bd1b17b4de66926108a7dfdf143e")]
-public class LoginPasswordExpiredDialog : FTOptix.UI.Dialog
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "44518ba0dcfcec4f95ece2f35ced3138")]
+public class _8_PDF_Selector : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "b6239b77b31bba48a937ee481e521c37")]
-public class LocaleComboBox : FTOptix.UI.ComboBox
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "a6e0276b8916d1e37f53e903cf2f4187")]
-public class _6_End_Disk_Timing : FTOptix.UI.Screen
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "0b9ba095ab72831d0e80abaa9b14aa42")]
+public class _1_E_Stop_Status : FTOptix.UI.Screen
 {
 }
 
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "410c2e22fda89b3c37b37363e4cae3b7")]
 public class Screen_Login : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "dfd608856ac9169edc3dd3847ce7f7bc")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "52b045d520e151468cb70a63d1b67c66")]
+public class Footer_Navigation1 : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "72af352b5396ce47d1f7e3293b653c5b")]
+public class Panel_Screen_Heater_Control : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "4d7abd18b3a74f6037333cb23cbdd47f")]
+public class _3_ScreenHeaterRunControlDisplay : FTOptix.UI.Screen
 {
 }
 
@@ -75,13 +75,13 @@ public class Screen_Forgot_password : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "5bd1738d3f60d47ba30b9df411fa0050")]
-public class EditUserDetailPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "a0205e99f62af411bca106354d5c02ca")]
+public class Image_Popup : FTOptix.UI.Dialog
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "a0205e99f62af411bca106354d5c02ca")]
-public class Image_Popup : FTOptix.UI.Dialog
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "b6239b77b31bba48a937ee481e521c37")]
+public class LocaleComboBox : FTOptix.UI.ComboBox
 {
 }
 
@@ -90,23 +90,8 @@ public class Button_1_pdf_view : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "2eebfd3def044e747aa57952fc13c70d")]
-public class Logout : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "0b9ba095ab72831d0e80abaa9b14aa42")]
-public class _1_E_Stop_Status : FTOptix.UI.Screen
-{
-}
-
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "906b659156e3c4bdde7772cb7b7f6a1b")]
 public class _2_OverloadFaultDisplay : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "fa074007f5e91b373c47dde38646d0c6")]
-public class _5_screen_heater_control : FTOptix.UI.Screen
 {
 }
 
@@ -115,8 +100,23 @@ public class _4_MachineSpeedPositionDisplay : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "76c6b9dc24666b9297ce397d3fb2b9fe")]
-public class CreateUserPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
+public class LoginChangePasswordForm : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "fa074007f5e91b373c47dde38646d0c6")]
+public class _5_screen_heater_control : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "a6e0276b8916d1e37f53e903cf2f4187")]
+public class _6_End_Disk_Timing : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
+public class LAN : FTOptix.UI.Panel
 {
 }
 
@@ -125,18 +125,28 @@ public class _7_Alarm_History : FTOptix.UI.Screen
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "343e9a553a1648e3de95b91f6d877b43")]
-public class DeviceSettingsWidget : FTOptix.UI.Screen
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "44518ba0dcfcec4f95ece2f35ced3138")]
-public class _8_PDF_Selector : FTOptix.UI.Screen
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
 {
 }
 
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "41382c57dc616862c1af276ff6942abc")]
 public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
+public class NetworkSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "f0afd51b3f49febd40b32153974e81b8")]
+public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
 {
 }
 
@@ -165,13 +175,18 @@ public class UserEditorOverview : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "59f233a86b88133bc07cecf82da51463")]
-public class GroupLabel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "638838acd9f5ae93ca75b058c300b43b")]
+public class NoUsersPanel : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "638838acd9f5ae93ca75b058c300b43b")]
-public class NoUsersPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "5bd1738d3f60d47ba30b9df411fa0050")]
+public class EditUserDetailPanel : FTOptix.UI.Panel
 {
 }
 
@@ -180,28 +195,18 @@ public class GroupsPanel : FTOptix.UI.Panel
 {
 }
 
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "76c6b9dc24666b9297ce397d3fb2b9fe")]
+public class CreateUserPanel : FTOptix.UI.Panel
+{
+}
+
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "937bdaeca748212659105fb563595c0b")]
 public class GroupCheckbox : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
-public class LoginChangePasswordForm : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
-public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "548a589cc1532b072d9d24316f7b6090")]
-public class Product_Information : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
-public class WAN : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "59f233a86b88133bc07cecf82da51463")]
+public class GroupLabel : FTOptix.UI.Panel
 {
 }
 
@@ -215,23 +220,13 @@ public class Diagnostics : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "e348b14dbc7035688714c7ced8a72128")]
-public class ConnectionPanel : FTOptix.UI.Panel
-{
-}
-
 [MapType(NamespaceUri = "Strutz_HMI", Guid = "ce814fddddd607c776a3b9a6e0b90c27")]
 public class Login : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "c05a81edcb91fa074772f19543ea1b38")]
-public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
-{
-}
-
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
-public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
 {
 }
 
@@ -240,13 +235,18 @@ public class RequestAssistancePanel : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "f0afd51b3f49febd40b32153974e81b8")]
-public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
 {
 }
 
-[MapType(NamespaceUri = "Strutz_HMI", Guid = "323ecbd80fe0dccc66307959b261f54a")]
-public class EmptyPanel : FTOptix.UI.Panel
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "53b1bd1b17b4de66926108a7dfdf143e")]
+public class LoginPasswordExpiredDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "Strutz_HMI", Guid = "2eebfd3def044e747aa57952fc13c70d")]
+public class Logout : FTOptix.UI.Panel
 {
 }
 
