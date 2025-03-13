137541ad5c291e6637dc276a1840f2f4991be19d
Strutz-Design <engineering@strutz.com>
2025-03-13 16:28:58 -0400

    Change speed secure display to blue

--- ProjectFiles/NetSolution/obj/Debug/Strutz_HMI.AssemblyInfo.cs
+++ ProjectFiles/NetSolution/obj/Debug/Strutz_HMI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strutz_HMI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d415dd76b9a1029f8ccb1869d9bb88bd9ca6069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7680ad19d63175e9d1c939c66ea60335e4aabb0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strutz_HMI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strutz_HMI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
